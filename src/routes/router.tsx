7316afa87912e6f11f177751ae73a25f94bba150
Victor <vhduartelima@hotmail.com>
2024-07-04 15:15:01 -0300

    feat: added router

---
+++ src/routes/router.tsx
@@ -0,0 +1,41 @@
+import App from 'components/App'
+import {
+  Route,
+  createBrowserRouter,
+  createRoutesFromElements
+} from 'react-router-dom'
+
+export const router = createBrowserRouter(
+  createRoutesFromElements(
+    //father route can be used to define a layout for the app
+    <>
+      {/* <Route>
+        <Route
+          path="/login"
+          // loader={({ params }: any) => {
+          //   console.log(params["lang"]);
+          //   //should return value
+          //   return null;
+          // }}
+          // action={() => {
+          //   return null;
+          // }}
+          // <Route path="/project/task?/:taskId" />
+
+          element={<Login />}
+          // errorElement={<GenericErrorPage />}
+        />
+      </Route> */}
+      <Route>
+        <Route
+          path="/"
+          element={<App />}
+          // errorElement={<GenericErrorPage />}
+          // handle={{
+          //   crumb: () => <Link to="/login">Messages</Link>,
+          // }}
+        />
+      </Route>
+    </>
+  )
+)
