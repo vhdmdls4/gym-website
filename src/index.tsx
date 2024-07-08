d93a8c743c3ded3186cca5482e4d9cad9c7d91eb
Victor <vhduartelima@hotmail.com>
2024-07-08 18:15:57 -0300

    feat: dark theme added and organize some folders and files

--- src/index.tsx
+++ src/index.tsx
@@ -11,7 +11,7 @@ const root = createRoot(container)
 
 root.render(
   <React.StrictMode>
-    <Theme>
+    <Theme appearance="dark" radius="full">
       <RouterProvider router={router} />
     </Theme>
   </React.StrictMode>
--- src/layout/App.tsx
+++ src/layout/App.tsx
@@ -13,7 +13,7 @@ function App() {
     <div className="relative overflow-hidden">
       <AppHeader />
 
-      <div className="h-screen sm:pb-40 sm:pt-24 lg:pb-48 lg:pt-40">
+      <div className="h-full sm:pb-40 sm:pt-24 lg:pb-48 lg:pt-40">
         <main className="relative mx-auto max-w-7xl px-4 sm:static sm:px-6 lg:px-8">
           <Outlet />
         </main>
