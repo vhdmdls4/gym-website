4191717d59f2250c3ef48ebc1d5ca6063a4ca8dd
Victor <vhduartelima@hotmail.com>
2024-07-14 23:04:12 -0300

    style: layout now having a padding top as initial value and a min-h-dvh

--- src/layout/App.tsx
+++ src/layout/App.tsx
@@ -1,6 +1,9 @@
 import { Outlet } from 'react-router-dom'
 import AppFooter from 'layout/AppFooter'
 import AppHeader from 'layout/AppHeader'
+import Sidebar from './components/Sidebar'
+import { Button } from '@radix-ui/themes'
+import { useState } from 'react'
 
 const randoms = [
   [1, 2],
@@ -10,10 +13,9 @@ const randoms = [
 
 function App() {
   return (
-    <div className="relative overflow-hidden">
+    <div className="relative overflow-hidden overflow-y-auto min-h-dvh">
       <AppHeader />
-
-      <div className="h-full sm:pb-40 sm:pt-10 lg:pb-48 lg:pt-20">
+      <div className="h-full sm:pb-40 pt-5 sm:pt-10 lg:pb-48 lg:pt-20">
         <main className="relative mx-auto max-w-7xl px-4 sm:static sm:px-6 lg:px-8">
           <Outlet />
         </main>
