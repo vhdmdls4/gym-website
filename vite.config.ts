7ad0c865b45e435d0da9a1ce8035bc42e35704b6
Victor <vhduartelima@hotmail.com>
2024-11-16 05:02:51 -0300

    config/lib: working out on shadcn lib

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import tsconfigPaths from 'vite-tsconfig-paths'
+import path from 'path'
 
 // https://vitejs.dev/config https://vitest.dev/config
 export default defineConfig({
@@ -12,5 +13,10 @@ export default defineConfig({
     environment: 'happy-dom',
     setupFiles: '.vitest/setup',
     include: ['**/test.{ts,tsx}']
-  }
+  },
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
 })
