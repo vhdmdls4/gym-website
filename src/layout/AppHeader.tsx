f8ced1de08d5929df510d32d81edc75d8e5c2649
Victor <vhduartelima@hotmail.com>
2024-11-25 21:42:42 -0300

    update: app header

--- src/layout/AppHeader.tsx
+++ src/layout/AppHeader.tsx
@@ -3,10 +3,12 @@ import TabsMenu from './components/TabsMenu'
 import { GiWeightLiftingUp } from 'react-icons/gi'
 import Sidebar from './components/Sidebar'
 import Show from '@/components/solidjs/Show'
+import { Link, useNavigate } from 'react-router-dom'
 
 type Props = {}
 
 export default function AppHeader({}: Props) {
+  const navigate = useNavigate()
   return (
     <header className="relative mx-auto max-w-7xl px-4 sm:static sm:px-6 lg:px-8 pt-5">
       <Flex justify="between">
@@ -22,7 +24,13 @@ export default function AppHeader({}: Props) {
           <Sidebar />
         </div>
         <div className="hidden lg:block">
-          <Button size="4" variant="solid" highContrast color="gray">
+          <Button
+            size="4"
+            variant="solid"
+            highContrast
+            color="gray"
+            onClick={() => navigate('/gym-website/join-us')}
+          >
             Register
           </Button>
         </div>
