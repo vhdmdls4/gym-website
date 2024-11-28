a9391d1866209e2344f78e32e5857b59c907e777
Victor <vhduartelima@hotmail.com>
2024-11-27 23:13:55 -0300

    feat: added why us, join us and about us

--- src/domains/Home/index.tsx
+++ src/domains/Home/index.tsx
@@ -1,12 +1,18 @@
 import { Flex } from '@radix-ui/themes'
 import Hero from '../Hero'
 import Plans from '../Plans'
+import WhyUs from '../WhyUs'
+import { JoinUs } from '../JoinUs'
+import AboutUs from '../AboutUs'
 
 export default function Home() {
   return (
-    <Flex direction={'column'}>
+    <Flex direction={'column'} gapY={'8'}>
       <Hero />
       <Plans />
+      <WhyUs />
+      <JoinUs />
+      <AboutUs />
     </Flex>
   )
 }
