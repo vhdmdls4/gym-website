e9bf06089fd1e141b82faaf2f6979772e4f693e5
Victor <vhduartelima@hotmail.com>
2024-11-28 00:22:43 -0300

    wip: hero

--- src/domains/Hero/index.tsx
+++ src/domains/Hero/index.tsx
@@ -5,19 +5,22 @@ import PartnersSection from './components/PartnersSection'
 
 export default function Hero() {
   return (
-    <Grid
-      columns={{
-        initial: '1',
-        lg: '2'
-      }}
-      gap="5"
-      width="auto"
-      gridColumn={{ initial: '1', lg: '2' }}
-      gridRow={{ initial: '3', lg: '2' }}
-    >
-      <HeroSection />
-      <PartnersSection />
-      <BannerSection />
-    </Grid>
+    <div className="min-h-dvh">
+      <Grid
+        columns={{
+          initial: '1',
+          lg: '2'
+        }}
+        gap="5"
+        width="auto"
+        gridColumn={{ initial: '1', lg: '2' }}
+        gridRow={{ initial: '3', lg: '2' }}
+        height={{ initial: 'auto', lg: 'h-full' }}
+      >
+        <HeroSection />
+        <PartnersSection />
+        <BannerSection />
+      </Grid>
+    </div>
   )
 }
